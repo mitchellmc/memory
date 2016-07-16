96770b703d652edbc4d7c3cef139762d2a532925
En Fuego <aggressionred@gmail.com>
2016-07-16 17:33:34 -0400

    change http server file name

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+//import React from 'react'
+//import ReactDOM from 'react-dom'
+//import { createStore } from 'redux'
+
+//const store = createStore(counter)
+//const rootEl = document.getElementById('app-content')
+
+//render() {
+//  ReactDOM.render(
+//    <div>no</div>,
+//    rootEl
+//  );
+//}
+
+//render();
+//store.subscribe(render)
+//import React, { Component, PropTypes } from 'react'
+
+//class App extends Component {
+//  constructor(props) {
+//    super(props);
+//  };
+
+//  render(){
+//    return(
+//      <div>No</div>
+//    );
+//  }
+
+//  App.PropTypes = {
+//    name: PropTypes.string,
+//  };
+//};
+
+//export default App;
