685953ead0623fb4e731bf133c5d523a3aec631f
En Fuego <aggressionred@gmail.com>
2016-08-02 09:02:26 -0400

    Add web pack hot-module-replacement , and react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-  entry: './src/main.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/main.js'
+  ],
   output: {
-    filename: './build/bundle.js'
+    filename: 'bundle.js',
+    path: __dirname + '/build'
   },
   module: {
-    loaders : [
+    loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loaders: ['react-hot', 'babel-loader?presets[]=es2015,presets[]=stage-0,presets[]=react'],
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react', 'stage-0']
-        }
+        include: path.join(__dirname, 'src')
+        // query: {
+        //   presets: ['es2015', 'react', 'stage-0']
+        // }
       }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.json']
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
