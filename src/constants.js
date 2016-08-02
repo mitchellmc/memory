271b31cb6522eccc3a17a53a708d9223c9b419ea
En Fuego <aggressionred@gmail.com>
2016-08-02 19:59:32 -0400

    add new constants for card flipping

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,6 @@ export const SET_BOARD_20 = 'SET_BOARD_20';
 export const SET_BOARD_30 = 'SET_BOARD_30';
 export const TURN_CARD = 'TURN_CARD';
 export const CLEAR_OPEN_CARDS = 'CLEAR_OPEN_CARDS';
+export const FACING_DOWN = 'FACING_DOWN';
+export const FACING_UP = 'FACING_UP';
+export const SOLVED = 'SOLVED';
